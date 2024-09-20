492a7d1d5f1f0914c36dec722bec19ed84b04923
khatibomar <elkhatibomar@outlook.com>
2024-09-21 02:28:02 +0400

    modify treesitter to support Go

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,10 +5,26 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        -- Enable automatic installation of language parsers
         auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+
+        -- Specify which parsers to ensure are installed
+        ensure_installed = {
+          "go",         -- Go language parser
+        },
+
+        -- Enable syntax highlighting using Treesitter
+        highlight = {
+          enable = true,               -- Enable Treesitter-based highlighting
+          additional_vim_regex_highlighting = false,
+        },
+
+        -- Enable indentation based on Treesitter parsing
+        indent = {
+          enable = true,
+        },
       })
     end
   }
 }
+
