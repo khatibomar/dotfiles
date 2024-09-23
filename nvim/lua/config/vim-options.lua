672c11c654c47081aac50463a25d3ba042389b5e
khatibomar <elkhatibomar@outlook.com>
2024-09-23 17:13:02 +0400

    change colors of tokens

--- nvim/lua/config/vim-options.lua
+++ nvim/lua/config/vim-options.lua
@@ -9,3 +9,12 @@ vim.opt.clipboard:append("unnamedplus")
 -- Color scheme
 vim.o.termguicolors = true
 vim.cmd [[silent! colorscheme leaf-light]]
+vim.cmd [[highlight String guifg=#77B1B1]]
+vim.cmd [[highlight Comment guifg=#6E8D6E]]
+vim.cmd [[highlight Function guifg=#9D82A9]]
+vim.cmd [[highlight Keyword guifg=#D1A66A]]
+vim.cmd [[highlight Identifier guifg=#5F9EB1]]
+vim.cmd [[highlight Number guifg=#89C3B3]]
+vim.cmd [[highlight Boolean guifg=#A75E5E]]
+vim.cmd [[highlight Operator guifg=#737373]]
+vim.cmd [[highlight Type guifg=#846C92]]
