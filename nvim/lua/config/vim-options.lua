6e79b61851225a713920c539114225a2125c845a
khatibomar <elkhatibomar@outlook.com>
2024-09-24 21:19:51 +0400

    enable line number and relative number

--- nvim/lua/config/vim-options.lua
+++ nvim/lua/config/vim-options.lua
@@ -1,20 +1,26 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd([[ 
+    set number
+    set relativenumber
+    set expandtab
+    set tabstop=2
+    set softtabstop=2
+    set shiftwidth=2
+]])
 
 -- Enable copy to clipboard
 vim.opt.clipboard:append("unnamedplus")
+vim.opt.termguicolors = true
 
 -- Color scheme
-vim.o.termguicolors = true
-vim.cmd [[silent! colorscheme leaf-light]]
-vim.cmd [[highlight String guifg=#77B1B1]]
-vim.cmd [[highlight Comment guifg=#6E8D6E]]
-vim.cmd [[highlight Function guifg=#9D82A9]]
-vim.cmd [[highlight Keyword guifg=#D1A66A]]
-vim.cmd [[highlight Identifier guifg=#5F9EB1]]
-vim.cmd [[highlight Number guifg=#89C3B3]]
-vim.cmd [[highlight Boolean guifg=#A75E5E]]
-vim.cmd [[highlight Operator guifg=#737373]]
-vim.cmd [[highlight Type guifg=#846C92]]
+vim.cmd([[
+  silent! colorscheme leaf-light
+  highlight String guifg=#77B1B1
+  highlight Comment guifg=#6E8D6E
+  highlight Function guifg=#9D82A9
+  highlight Keyword guifg=#D1A66A
+  highlight Identifier guifg=#5F9EB1
+  highlight Number guifg=#89C3B3
+  highlight Boolean guifg=#A75E5E
+  highlight Operator guifg=#737373
+  highlight Type guifg=#846C92
+]])
