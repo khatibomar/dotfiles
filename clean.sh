57ad4cbe97ecc774d6d2a5fec6ec921fa095d924
khatibomar <elkhatibomar@outlook.com>
2024-09-29 18:37:05 +0400

    update clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# Clean nvim backups
 echo "Starting nvim backups cleaning"
-rm -rf $HOME/.config/nvim-*
+rm -rf "$HOME/.config-backup-*"  # Adjusted to match the backup naming scheme
 echo "Cleaning nvim backups completed successfully."
+
