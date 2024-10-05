5a6b55d5d1a4fcc9def3e5f40f932fca75046eca
khatibomar <elkhatibomar@outlook.com>
2024-10-05 22:45:43 +0400

    add htop config

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -11,55 +11,66 @@ BACKUP_DIR="$HOME/.config-backup-$current_date"
 mkdir -p "$BACKUP_DIR"
 
 # Define an exclusion list for the general config files
-EXCLUDED_FILES=("gitconfig" "alacritty.toml")
+EXCLUDED_FILES=("gitconfig" "alacritty.toml" "htoprc")
 
 # Backup and copy general config files
 if [ -d "$CONFIG_DIR" ]; then
-  for item in "$CONFIG_DIR"/*; do
-    # Check if there are no files in CONFIG_DIR
-    if [ ! -e "$item" ]; then
-      echo "No config files found in $CONFIG_DIR."
-      break
-    fi
+	for item in "$CONFIG_DIR"/*; do
+		# Check if there are no files in CONFIG_DIR
+		if [ ! -e "$item" ]; then
+			echo "No config files found in $CONFIG_DIR."
+			break
+		fi
 
-    # Get the base name of the item
-    base_name=$(basename "$item")
+		# Get the base name of the item
+		base_name=$(basename "$item")
 
-    # Check if the item is in the exclusion list
-    if [[ " ${EXCLUDED_FILES[@]} " =~ " $base_name " ]]; then
-      echo "Skipping excluded item: $base_name"
-      continue
-    fi
+		# Check if the item is in the exclusion list
+		if [[ " ${EXCLUDED_FILES[@]} " =~ " $base_name " ]]; then
+			echo "Skipping excluded item: $base_name"
+			continue
+		fi
 
-    OLD_FILE="$HOME/.$base_name"
-    BACKUP_FILE="$BACKUP_DIR/$base_name"
+		OLD_FILE="$HOME/.$base_name"
+		BACKUP_FILE="$BACKUP_DIR/$base_name"
 
-    # Backup the old file or directory if it exists
-    if [ -e "$OLD_FILE" ]; then
-      echo "Backing up the old .$base_name to $BACKUP_FILE"
-      mv "$OLD_FILE" "$BACKUP_FILE"
-    else
-      echo "No existing .$base_name found. No backup needed."
-    fi
+		# Backup the old file or directory if it exists
+		if [ -e "$OLD_FILE" ]; then
+			echo "Backing up the old .$base_name to $BACKUP_FILE"
+			mv "$OLD_FILE" "$BACKUP_FILE"
+		else
+			echo "No existing .$base_name found. No backup needed."
+		fi
 
-    # Copy the new item to the HOME directory with a . prefix
-    echo "Copying the new $base_name to $OLD_FILE"
-    cp -r "$item" "$OLD_FILE"
-  done
+		# Copy the new item to the HOME directory with a . prefix
+		echo "Copying the new $base_name to $OLD_FILE"
+		cp -r "$item" "$OLD_FILE"
+	done
 else
-  echo "Error: Config directory $CONFIG_DIR does not exist."
-  exit 1
+	echo "Error: Config directory $CONFIG_DIR does not exist."
+	exit 1
 fi
 
 # Handle alacritty.toml separately
 if [ -f "$CONFIG_DIR/alacritty.toml" ]; then
-  ALACRITTY_DIR="$HOME/.config/alacritty"
-  mkdir -p "$ALACRITTY_DIR" # Create the alacritty directory if it doesn't exist
-  echo "Copying alacritty.toml to $ALACRITTY_DIR/alacritty.toml"
-  cp "$CONFIG_DIR/alacritty.toml" "$ALACRITTY_DIR/alacritty.toml"
+	ALACRITTY_DIR="$HOME/.config/alacritty"
+	mkdir -p "$ALACRITTY_DIR" # Create the alacritty directory if it doesn't exist
+	echo "Copying alacritty.toml to $ALACRITTY_DIR/alacritty.toml"
+	cp "$CONFIG_DIR/alacritty.toml" "$ALACRITTY_DIR/alacritty.toml"
 else
-  echo "Error: Config directory $CONFIG_DIR does not exist."
-  exit 1
+	echo "Error: Config directory $CONFIG_DIR does not exist."
+	exit 1
+fi
+
+# Handle htoprc separately
+if [ -f "$CONFIG_DIR/htoprc" ]; then
+	HTOP_DIR="$HOME/.config/htop"
+	mkdir -p "$HTOP_DIR" # Create the alacritty directory if it doesn't exist
+	echo "Copying htoprc to $HTOP_DIR/htoprc"
+	cp "$CONFIG_DIR/htoprc" "$HTOP_DIR/htoprc"
+else
+	echo "Error: Config directory $CONFIG_DIR does not exist."
+	exit 1
 fi
 
 echo "Backup and copy for general config completed successfully."
