c136717b412b5431e45b80abb2e951085b6e5920
khatibomar <elkhatibomar@outlook.com>
2024-10-06 22:06:04 +0400

    Revert "hide confirmation message after quit popup"
    This reverts commit ee4f11137863acd069a06940a4d83ac1ccd6ea4c.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 require("config.keymaps.go-definition")
 
 vim.keymap.set('n', '<leader>s', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>p', ':execute "!tmux display-popup -E \\"cd ' .. vim.fn.getcwd() .. ' && zsh -c \\"exit\\"\\""<CR>', { noremap = true, silent = true })
-
+vim.keymap.set('n', '<Leader>p', ':!tmux display-popup -E "$SHELL"<CR>', { noremap = true, silent = true })
