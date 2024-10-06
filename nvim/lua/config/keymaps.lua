9dc865bbd109c437bb51169571e9c4abf2cba730
khatibomar <elkhatibomar@outlook.com>
2024-10-06 22:20:27 +0400

    change approach to lunch a popup from nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,10 @@
 require("config.keymaps.go-definition")
 
+function OpenTmuxPopup()
+  local cmd = 'tmux display-popup -E "cd ' .. vim.fn.getcwd() .. ' && $SHELL "'
+  vim.fn.system(cmd)
+end
+
 vim.keymap.set('n', '<leader>s', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>p', ':lua OpenTmuxPopup()<CR>', { noremap = true, silent = true })
 
