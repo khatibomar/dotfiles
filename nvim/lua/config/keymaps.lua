7723d3a3d2f7778a64c6e0d2b940472c79db5334
khatibomar <elkhatibomar@outlook.com>
2024-10-06 21:33:24 +0400

    add pop up inside neovim too

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 require("config.keymaps.go-definition")
 
 vim.keymap.set('n', '<leader>s', ':w<CR>', { noremap = true, silent = true })
-
+vim.keymap.set('n', '<Leader>p', ':!tmux display-popup -E "$SHELL"<CR>', { noremap = true, silent = true })
