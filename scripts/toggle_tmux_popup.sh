a057603def0e9ee560fe2cab2e8055ba917f0401
khatibomar <elkhatibomar@outlook.com>
2024-10-25 18:26:51 +0400

    open unique popup per pane

--- scripts/toggle_tmux_popup.sh
+++ scripts/toggle_tmux_popup.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
-#! /usr/bin/env bash
+# Generate a unique session name based on the pane ID
+tmux_popup_session_name="_popup_$(tmux display-message -p -F "#{pane_id}")"
 
-tmux_popup_session_name="_popup"
-
-if [ "$(tmux display-message -p -F "#{session_name}")" = "${tmux_popup_session_name}" ];then
+# Check if the current session name contains "popup"
+if [[ "$(tmux display-message -p -F "#{session_name}")" == *"_popup_"* ]]; then
+    # If we're already in a popup, detach instead of creating another one
     tmux detach-client
 else
-    tmux popup -d '#{pane_current_path}' -xC -yC -w80% -h80% -E\
+    # Open a new popup session with the unique name for the current pane
+    tmux popup -d '#{pane_current_path}' -xC -yC -w80% -h80% -E \
       "tmux new-session -A -s ${tmux_popup_session_name}"
 fi
+
