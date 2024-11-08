559d594696e4d74fa8e4dac2a0c331350379eb9f
khatibomar <elkhatibomar@outlook.com>
2024-11-08 16:22:36 +0400

    auto format lua on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,30 +1,28 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- Lua formatting
-        null_ls.builtins.formatting.stylua,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Lua formatting
+				null_ls.builtins.formatting.stylua,
 
-        -- Go formatting
-        null_ls.builtins.formatting.gofmt,     -- for formatting with gofmt
-        null_ls.builtins.formatting.goimports, -- for formatting with goimports
+				-- Go formatting
+				null_ls.builtins.formatting.gofmt, -- for formatting with gofmt
+				null_ls.builtins.formatting.goimports, -- for formatting with goimports
 
-        -- Go linting
-        null_ls.builtins.diagnostics.golangci_lint.with({
-          command = "golangci-lint", -- adjust the command path if necessary
-        }),
+				-- Go linting
+				null_ls.builtins.diagnostics.golangci_lint.with({
+					command = "golangci-lint", -- adjust the command path if necessary
+				}),
 
-        -- Bash support
-        null_ls.builtins.formatting.shfmt,      -- for Bash formatting
-        null_ls.builtins.diagnostics.shellcheck -- for Bash linting
-      },
-    })
+				-- Bash support
+				null_ls.builtins.formatting.shfmt, -- for Bash formatting
+				null_ls.builtins.diagnostics.shellcheck, -- for Bash linting
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    vim.cmd([[autocmd BufWritePre *.go,*.sh lua vim.lsp.buf.format()]])
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.cmd([[autocmd BufWritePre *.go,*.sh,*.lua lua vim.lsp.buf.format()]])
+	end,
 }
-
-
