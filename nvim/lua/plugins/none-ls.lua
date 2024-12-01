1ff808beaeddc119157d4eb7199c148fcad92c34
khatibomar <elkhatibomar@outlook.com>
2024-12-01 23:16:13 +0400

    fix format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,29 +1,40 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- Lua formatting
-        null_ls.builtins.formatting.stylua,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-        -- Go support with gopls for imports, formatting, and code actions
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.formatting.goimports_reviser,
+		null_ls.setup({
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+			sources = {
+				-- Lua formatting
+				null_ls.builtins.formatting.stylua,
 
-        -- Bash support
-        null_ls.builtins.formatting.shfmt,   -- for Bash formatting
-        null_ls.builtins.diagnostics.shellcheck, -- for Bash linting
-      },
-    })
+				-- Go support with gopls for imports, formatting, and code actions
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.formatting.goimports_reviser,
 
-    -- Automatically format and fix imports on save for Go files
-    vim.cmd([[autocmd BufWritePre *.go lua vim.lsp.buf.format()]])
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+				-- Bash support
+				null_ls.builtins.formatting.shfmt, -- for Bash formatting
+				null_ls.builtins.diagnostics.shellcheck, -- for Bash linting
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
-
