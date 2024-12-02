8cb7ca65e791f150efbce16007aa265185f1bbee
khatibomar <elkhatibomar@outlook.com>
2024-12-02 05:15:17 +0400

    improve code formatting and readability
    Issue #2
    * Align key mappings for better visual organization
    * Convert single quotes to double quotes for consistency
    * Add additional file ignore patterns (.pb.go, .idea, .run)
    * Improve code formatting with proper indentation and line wrapping
    * Remove trailing empty line

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,45 +8,54 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       -- Find files including hidden and ignored files
-      { "<leader><leader>", "<cmd>Telescope find_files<cr>", desc = "Find Files (including hidden and ignored)" },
+      {
+        "<leader><leader>",
+        "<cmd>Telescope find_files<cr>",
+        desc = "Find Files (including hidden and ignored)",
+      },
       -- Other key bindings
-      { "<leader>lb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>rf", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
-      { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search in Current Buffer" },
-      { "<leader>lg", "<cmd>Telescope live_grep<cr>", desc = "Search Text in Workspace" },
+      { "<leader>lb", "<cmd>Telescope buffers<cr>",                   desc = "Buffers" },
+      { "<leader>rf", "<cmd>Telescope oldfiles<cr>",                  desc = "Recent Files" },
+      { "<leader>/",  "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search in Current Buffer" },
+      { "<leader>lg", "<cmd>Telescope live_grep<cr>",                 desc = "Search Text in Workspace" },
     },
     config = function()
       require("telescope").setup({
         defaults = {
           -- Apply global options for all pickers
           vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
           },
-          file_ignore_patterns = {   -- Exclude node_modules and .git directories
+          file_ignore_patterns = {
             "node_modules",
             ".git",
+            "%.pb%.go",
+            ".idea",
+            ".run",
           },
           -- Apply hidden and no_ignore globally
-          hidden = true,             -- Include hidden files by default
-          no_ignore = true,          -- Include ignored files by .gitignore
-          no_ignore_parent = true,   -- Include ignored files from parent .gitignore
+          hidden = true,      -- Include hidden files by default
+          no_ignore = true,   -- Include ignored files by .gitignore
+          no_ignore_parent = true, -- Include ignored files from parent .gitignore
         },
         pickers = {
           -- Ensure find_files uses these options
           find_files = {
-            hidden = true,             -- Include hidden files
-            no_ignore = true,          -- Include ignored files
-            no_ignore_parent = true,   -- Include parent .gitignore
+            hidden = true,     -- Include hidden files
+            no_ignore = true,  -- Include ignored files
+            no_ignore_parent = true, -- Include parent .gitignore
           },
           -- Ensure live_grep uses these options
           live_grep = {
-            additional_args = function() return { "--hidden", "--no-ignore" } end, -- Include hidden and ignored files
+            additional_args = function()
+              return { "--hidden", "--no-ignore" }
+            end, -- Include hidden and ignored files
           },
         },
         extensions = {
@@ -60,4 +69,3 @@ return {
     end,
   },
 }
-
