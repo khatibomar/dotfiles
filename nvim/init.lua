190f2ed666eb290ed460c0a57537ba3bfc4470fd
khatibomar <elkhatibomar@outlook.com>
2024-12-29 16:50:40 +0200

    add icons configuration and improve vim options setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,13 @@
--- Set leader key to space
-vim.g.mapleader = " "
+local g = vim.g
+
+-- Leader keys
+g.mapleader = " "
+g.maplocalleader = " "
 
 require("config.lazy")
 require("config.vim-options")
 require("config.diagnostic")
 require("config.keymaps")
 require("config.signs")
+require("config.icons")
 require("config.telescope.makefile_targets")
--- nvim/lua/config/vim-options.lua
+++ nvim/lua/config/vim-options.lua
@@ -1,38 +1,74 @@
-vim.cmd([[
-    set number
-    set relativenumber
-    set expandtab
-    set cursorline
+local opt = vim.opt
+local api = vim.api
 
-    set tabstop=2
-    set softtabstop=2
-    set shiftwidth=2
-    colorscheme leaf-light
-]])
+-- General options
+opt.number = true                               -- Show line numbers
+opt.relativenumber = true                       -- Show relative line numbers
+opt.cursorline = false                          -- Disable current line highlighting
+opt.wrap = false                                -- Disable line wrapping
+opt.breakindent = true                          -- Wrapped lines will respect indentation
+opt.scrolloff = 8                               -- Minimum lines above/below cursor
+opt.sidescrolloff = 8                           -- Minimum columns left/right of cursor
+opt.signcolumn = "yes"                          -- Always show sign column
+opt.numberwidth = 4                             -- Set width of the number column
+opt.splitbelow = true                           -- Horizontal splits go below current window
+opt.splitright = true                           -- Vertical splits go to the right of current window
+opt.title = true                                -- Enable title in window title bar
+opt.confirm = true                              -- Ask for confirmation when closing modified buffers
+opt.showcmd = false                             -- Disable command display in the last line
+opt.ruler = false                               -- Disable ruler
+opt.fillchars = { eob = " " }                   -- Use space for empty lines
+opt.clipboard:append("unnamedplus")             -- Use system clipboard
+opt.termguicolors = true                        -- Enable 24-bit colors
+opt.updatetime = 100                            -- Reduce update time for completion
+opt.timeoutlen = 1000                           -- Time to wait for mapped sequence
 
--- Enable copy to clipboard
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.termguicolors = true
+-- Indentation options
+opt.expandtab = true                            -- Convert tabs to spaces
+opt.tabstop = 2                                 -- Number of spaces per tab
+opt.softtabstop = 2                             -- Number of spaces per soft tab
+opt.shiftwidth = 2                              -- Number of spaces per indent
+opt.breakindent = true                          -- Preserve indentation in wrapped lines
 
--- Enable autoread using the new API
-vim.opt.autoread = true
+-- File behavior options
+opt.backup = false                              -- Disable backup files
+opt.swapfile = false                            -- Disable swap files
+opt.writebackup = false                         -- Disable write backups
+opt.undofile = true                             -- Enable persistent undo
+opt.autoread = true                             -- Reload files changed outside of Neovim
 
--- Create a new autocmd group for managing changes
-local group = vim.api.nvim_create_augroup("AutoReadGroup", { clear = true })
+-- Colorscheme
+vim.cmd([[colorscheme leaf-light]])
 
--- Automatically check for file changes when gaining focus or entering a buffer
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-	group = group,
-	callback = function()
-		vim.cmd("checktime") -- Check if files have changed externally
-	end,
-	desc = "Check for file changes on focus or buffer enter",
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- Autocommand group for autoread
+local group = api.nvim_create_augroup("AutoReadGroup", { clear = true })
+api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+    group = group,
+    callback = function()
+        vim.cmd("checktime") -- Check if files have changed externally
+    end,
+    desc = "Check for file changes on focus or buffer enter",
 })
 
--- Check if diff mode is enabled
-if vim.opt.diff:get() then
-	-- Define key mappings for diff mode
-	vim.api.nvim_set_keymap("n", "<localleader>1", ":diffget LOCAL<CR>", { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<localleader>2", ":diffget BASE<CR>", { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<localleader>3", ":diffget REMOTE<CR>", { noremap = true, silent = true })
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,ar,jp"
+    end,
+  }
+)
+
+-- Diff mode key mappings
+if opt.diff:get() then
+    local diff_opts = { noremap = true, silent = true }
+    api.nvim_set_keymap("n", "<localleader>1", ":diffget LOCAL<CR>", diff_opts)
+    api.nvim_set_keymap("n", "<localleader>2", ":diffget BASE<CR>", diff_opts)
+    api.nvim_set_keymap("n", "<localleader>3", ":diffget REMOTE<CR>", diff_opts)
 end
