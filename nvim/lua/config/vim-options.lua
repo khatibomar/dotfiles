6b22b090dbadf5f6b3e6ed53217845b574cebcdc
khatibomar <elkhatibomar@outlook.com>
2025-01-21 04:49:11 +0200

    improve code formatting and indentation
    * Move format on save logic from none-ls to vim-options for better control
    * Add contextindent plugin for better indentation support
    * Clean up none-ls configuration and remove duplicate format-on-save logic
    * Align comments in vim-options.lua for better readability
    * Make ruler behavior consistent (was both enabled and disabled)

--- nvim/lua/config/vim-options.lua
+++ nvim/lua/config/vim-options.lua
@@ -2,42 +2,42 @@ local opt = vim.opt
 local api = vim.api
 
 -- General options
-opt.number = true -- Show line numbers
-opt.relativenumber = true -- Show relative line numbers
-opt.cursorline = false -- Disable current line highlighting
-opt.wrap = false -- Disable line wrapping
-opt.breakindent = true -- Wrapped lines will respect indentation
-opt.scrolloff = 8 -- Minimum lines above/below cursor
-opt.sidescrolloff = 8 -- Minimum columns left/right of cursor
-opt.signcolumn = "yes" -- Always show sign column
-opt.numberwidth = 4 -- Set width of the number column
-opt.splitbelow = true -- Horizontal splits go below current window
-opt.splitright = true -- Vertical splits go to the right of current window
-opt.title = true -- Enable title in window title bar
-opt.confirm = true -- Ask for confirmation when closing modified buffers
-opt.showcmd = false -- Disable command display in the last line
-opt.ruler = false -- Disable ruler
-opt.fillchars = { eob = " " } -- Use space for empty lines
+opt.number = true                   -- Show line numbers
+opt.relativenumber = true           -- Show relative line numbers
+opt.cursorline = false              -- Disable current line highlighting
+opt.wrap = false                    -- Disable line wrapping
+opt.breakindent = true              -- Wrapped lines will respect indentation
+opt.scrolloff = 8                   -- Minimum lines above/below cursor
+opt.sidescrolloff = 8               -- Minimum columns left/right of cursor
+opt.signcolumn = "yes"              -- Always show sign column
+opt.numberwidth = 4                 -- Set width of the number column
+opt.splitbelow = true               -- Horizontal splits go below current window
+opt.splitright = true               -- Vertical splits go to the right of current window
+opt.title = true                    -- Enable title in window title bar
+opt.confirm = true                  -- Ask for confirmation when closing modified buffers
+opt.showcmd = false                 -- Disable command display in the last line
+opt.ruler = false                   -- Disable ruler
+opt.fillchars = { eob = " " }       -- Use space for empty lines
 opt.clipboard:append("unnamedplus") -- Use system clipboard
-opt.termguicolors = true -- Enable 24-bit colors
-opt.updatetime = 100 -- Reduce update time for completion
-opt.timeoutlen = 1000 -- Time to wait for mapped sequence
-opt.ruler = true -- Disable ruler
-opt.rulerformat = "%l,%v" -- Set ruler format
+opt.termguicolors = true            -- Enable 24-bit colors
+opt.updatetime = 100                -- Reduce update time for completion
+opt.timeoutlen = 1000               -- Time to wait for mapped sequence
+opt.ruler = true                    -- Disable ruler
+opt.rulerformat = "%l,%v"           -- Set ruler format
 
 -- Indentation options
-opt.expandtab = true -- Convert tabs to spaces
-opt.tabstop = 2 -- Number of spaces per tab
-opt.softtabstop = 2 -- Number of spaces per soft tab
-opt.shiftwidth = 2 -- Number of spaces per indent
+opt.expandtab = true   -- Convert tabs to spaces
+opt.tabstop = 2        -- Number of spaces per tab
+opt.softtabstop = 2    -- Number of spaces per soft tab
+opt.shiftwidth = 2     -- Number of spaces per indent
 opt.breakindent = true -- Preserve indentation in wrapped lines
 
 -- File behavior options
-opt.backup = false -- Disable backup files
-opt.swapfile = false -- Disable swap files
+opt.backup = false      -- Disable backup files
+opt.swapfile = false    -- Disable swap files
 opt.writebackup = false -- Disable write backups
-opt.undofile = true -- Enable persistent undo
-opt.autoread = true -- Reload files changed outside of Neovim
+opt.undofile = true     -- Enable persistent undo
+opt.autoread = true     -- Reload files changed outside of NeoVim
 
 -- Colorscheme
 vim.cmd([[colorscheme leaf-light]])
@@ -48,26 +48,57 @@ api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 -- Autocommand group for autoread
 local group = api.nvim_create_augroup("AutoReadGroup", { clear = true })
 api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-	group = group,
-	callback = function()
-		vim.cmd("checktime") -- Check if files have changed externally
-	end,
-	desc = "Check for file changes on focus or buffer enter",
+  group = group,
+  callback = function()
+    vim.cmd("checktime") -- Check if files have changed externally
+  end,
+  desc = "Check for file changes on focus or buffer enter",
 })
 
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.txt", "*.md", "*.tex" },
-	callback = function()
-		vim.opt.spell = true
-		vim.opt.spelllang = "en_us" -- Arabic and Japanese are not supported
-	end,
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = "en_us" -- Arabic and Japanese are not supported
+  end,
 })
 
 -- Diff mode key mappings
 if opt.diff:get() then
-	local diff_opts = { noremap = true, silent = true }
-	api.nvim_set_keymap("n", "<localleader>1", ":diffget LOCAL<CR>", diff_opts)
-	api.nvim_set_keymap("n", "<localleader>2", ":diffget BASE<CR>", diff_opts)
-	api.nvim_set_keymap("n", "<localleader>3", ":diffget REMOTE<CR>", diff_opts)
+  local diff_opts = { noremap = true, silent = true }
+  api.nvim_set_keymap("n", "<localleader>1", ":diffget LOCAL<CR>", diff_opts)
+  api.nvim_set_keymap("n", "<localleader>2", ":diffget BASE<CR>", diff_opts)
+  api.nvim_set_keymap("n", "<localleader>3", ":diffget REMOTE<CR>", diff_opts)
 end
+
+-- Add a general autocommand for formatting on save
+api.nvim_create_autocmd("BufWritePre", {
+  group = group,
+  callback = function()
+    -- Use LSP formatting if available, otherwise use built-in formatting
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    local has_formatter = false
+    for _, client in pairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+      if client.supports_method("textDocument/formatting") then
+        has_formatter = true
+        break
+      end
+    end
+    if has_formatter then
+      -- Use LSP formatting
+      vim.lsp.buf.format()
+      print("Formatted with LSP")
+    else
+      -- This is used for plugin contextindent
+      print("Formatted with built-in")
+      local curpos = vim.fn.getpos(".")     -- Save cursor position
+      local winsaveview = vim.fn.winsaveview() -- Save screen view
+      vim.cmd("keepjumps normal! gg=G")     -- Run formatting
+      vim.fn.setpos(".", curpos)            -- Restore cursor position
+      vim.fn.winrestview(winsaveview)       -- Restore screen view
+    end
+  end,
+  desc = "Format buffer on save",
+})
