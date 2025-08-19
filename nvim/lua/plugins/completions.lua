c2cb33ec2e0be5d64f67cfaa136db7f90a59393d
ayn <ayn.khatib@gmail.com>
2025-08-19 03:51:54 +0300

    improve completion menu appearance and highlights
    - Added lspkind.nvim for enhanced completion item icons.
    - Configured blink.compat completion menu with custom padding and kind icon.
    - Set BlinkCmpMenuSelection highlight for better selection visibility.
    - Updated autocmds and formatting for consistency.
    - Improved diagnostic and grep settings for clarity.
    
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -3,10 +3,33 @@ return {
 	dependencies = {
 		{ "samiulsami/cmp-go-deep", dependencies = { "kkharji/sqlite.lua" } },
 		{ "saghen/blink.compat" },
+		{ "onsails/lspkind.nvim" },
 	},
 	version = "1.*",
 	opts = {
-		completion = { documentation = { auto_show = true } },
+		appearance = {
+			highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = { auto_show = true },
+			menu = {
+				draw = {
+					padding = { 0, 1 }, -- padding only on right side
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								return " " .. ctx.kind_icon .. ctx.icon_gap .. " "
+							end,
+						},
+					},
+				},
+			},
+		},
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 		keymap = { preset = "enter" },
 		sources = {
