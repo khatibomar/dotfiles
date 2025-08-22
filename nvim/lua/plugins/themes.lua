42fe06d2921b0290db354b5284e2538339ed75e3
ayn <ayn.khatib@gmail.com>
2025-08-22 20:07:30 +0300

    simplify configs and update theme setup
    Refactored oil.nvim plugin config for clarity and removed redundant
    keymaps and options. Updated themes.lua to streamline theme plugin
    setup and ensure consistent light style for both leaf and newpaper
    themes. Removed citruszest theme for a cleaner configuration.
    
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,32 +1,28 @@
 return {
-	{
-		"khatibomar/leaf",
-		config = function()
-			require("leaf").setup({
-				theme = "light",
-				transparent = false,
-				italics = {
-					comments = false,
-					keywords = false,
-					functions = false,
-					strings = false,
-					variables = false,
-				},
-			})
-		end,
-	},
-	{
-		"zootedb0t/citruszest.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"yorik1984/newpaper.nvim",
-		priority = 1000,
-		config = function()
-			require("newpaper").setup({
-				style = "light",
-			})
-		end,
-	},
+  {
+    "khatibomar/leaf",
+    config = function()
+      require("leaf").setup({
+        theme = "light",
+        transparent = false,
+        italics = {
+          comments = false,
+          keywords = false,
+          functions = false,
+          strings = false,
+          variables = false,
+        },
+        style = "light",
+      })
+    end,
+  },
+  {
+    "yorik1984/newpaper.nvim",
+    priority = 1000,
+    config = function()
+      require("newpaper").setup({
+        style = "light",
+      })
+    end,
+  },
 }
