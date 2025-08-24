801e0a691bb8b2e5e0b1623f35ff866fe6e791ba
ayn <ayn.khatib@gmail.com>
2025-08-24 22:49:30 +0300

    remove newpaper.nvim, clean up themes config
    Removed the newpaper.nvim theme plugin and simplified the themes
    configuration to only include leaf.nvim. This streamlines the theme
    management and reduces unnecessary complexity in the config file.
    
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,28 +1,16 @@
 return {
-  {
-    "khatibomar/leaf",
-    config = function()
-      require("leaf").setup({
-        theme = "light",
-        transparent = false,
-        italics = {
-          comments = false,
-          keywords = false,
-          functions = false,
-          strings = false,
-          variables = false,
-        },
-        style = "light",
-      })
-    end,
-  },
-  {
-    "yorik1984/newpaper.nvim",
-    priority = 1000,
-    config = function()
-      require("newpaper").setup({
-        style = "light",
-      })
-    end,
-  },
+	"khatibomar/leaf",
+	config = function()
+		require("leaf").setup({
+			theme = "light",
+			transparent = false,
+			italics = {
+				comments = false,
+				keywords = false,
+				functions = false,
+				strings = false,
+				variables = false,
+			},
+		})
+	end,
 }
