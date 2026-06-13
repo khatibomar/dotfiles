fe097a7275cb758f19b7683b71da2cf6a0655244
ayn <ayn.khatib@gmail.com>
2026-06-13 22:54:13 +0300

    add tsoding theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,36 +1,54 @@
 return {
-	{
-		"khatibomar/gopher.nvim",
-		priority = 1000,
-		config = function()
-			require("gopher").setup({
-				theme = "dark",
-				transparent = false,
-				italics = {
-					comments = true,
-					keywords = false,
-					functions = false,
-					strings = false,
-					variables = false,
-				},
-			})
-			require("gopher").colorscheme()
-		end,
-	},
-	{
-		"khatibomar/leaf",
-		config = function()
-			require("leaf").setup({
-				theme = "light",
-				transparent = false,
-				italics = {
-					comments = false,
-					keywords = false,
-					functions = false,
-					strings = false,
-					variables = false,
-				},
-			})
-		end,
-	},
+  {
+    "khatibomar/tsoding.nvim",
+    priority = 1000,
+    config = function()
+      require("tsoding").setup({
+        theme = "dark",
+        transparent = false,
+        italics = {
+          comments = true,
+          keywords = false,
+          functions = false,
+          strings = false,
+          variables = false,
+        },
+      })
+      require("tsoding").colorscheme()
+    end,
+  },
+  {
+    "khatibomar/gopher.nvim",
+    priority = 1000,
+    config = function()
+      require("gopher").setup({
+        theme = "dark",
+        transparent = false,
+        italics = {
+          comments = true,
+          keywords = false,
+          functions = false,
+          strings = false,
+          variables = false,
+        },
+      })
+      require("gopher").colorscheme()
+    end,
+  },
+  {
+    "khatibomar/leaf",
+    config = function()
+      require("leaf").setup({
+        theme = "light",
+        transparent = false,
+        italics = {
+          comments = false,
+          keywords = false,
+          functions = false,
+          strings = false,
+          variables = false,
+        },
+      })
+    end,
+  },
 }
