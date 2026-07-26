915b8986d8fa713a97fdfd50292dfd07fe5527b7
ayn <ayn.khatib@gmail.com>
2026-07-26 03:25:15 +0300

    Make git branch check async in statusline
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/config/statusline.lua
+++ nvim/lua/config/statusline.lua
@@ -1,18 +1,36 @@
 local M = {}
+local branch_cache = ""
+
+function M.update_branch()
+	vim.fn.jobstart({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, {
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			if data and data[1] and data[1] ~= "" and not data[1]:match("fatal:") then
+				branch_cache = " " .. data[1]
+			else
+				branch_cache = ""
+			end
+		end,
+	})
+end
 
 function M.branch()
-	local h = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
-	if not h then
-		return ""
-	end
-	local b = h:read("*a"):gsub("%s+$", "")
-	h:close()
-	return b ~= "" and " " .. b or ""
+	return branch_cache
 end
 
 function M.setup()
 	_G.branch = M.branch
 	vim.o.statusline = "%f %h%m%r %=%{v:lua.branch()} %l:%c"
+
+	local group = vim.api.nvim_create_augroup("StatusLineGit", { clear = true })
+	vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "BufWritePost" }, {
+		group = group,
+		callback = function()
+			M.update_branch()
+		end,
+	})
+
+	M.update_branch()
 end
 
 return M
