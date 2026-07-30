ee275e342bff21e6f8b78e990a8d258f4cd53293
ayn <ayn.khatib@gmail.com>
2026-07-30 13:43:23 +0300

    Migrate nvim-treesitter config to new main-branch API
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,35 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
+		lazy = false,
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ignore_install = {},
-				-- Enable automatic installation of language parsers
-				auto_install = true,
-				sync_install = false,
+			-- Specify which parsers to ensure are installed
+			require("nvim-treesitter").install({
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"lua",
+				"bash",
+				"cpp",
+				"proto",
+				"diff",
+				"markdown",
+			})
 
-				-- Specify which parsers to ensure are installed
-				ensure_installed = {
-					"go",
-					"gomod",
-					"gowork",
-					"gosum",
-					"lua",
-					"bash",
-					"cpp",
-					"proto",
-					"diff",
-					"markdown",
-				},
-
-				-- Enable syntax highlighting using Treesitter
-				highlight = {
-					enable = true, -- Enable Treesitter-based highlighting
-					additional_vim_regex_highlighting = false,
-				},
-
-				-- Enable indentation based on Treesitter parsing
-				indent = {
-					enable = true,
-				},
+			-- Automatically enable highlighting and indentation for any
+			-- filetype that has an installed parser (replaces the old
+			-- auto_install/highlight.enable/indent.enable options).
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(args.match) or args.match
+					if vim.treesitter.language.add(lang) then
+						vim.treesitter.start()
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
 			})
 		end,
 	},
