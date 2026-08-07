1b66eb5219bca8238e6ea16609dd80617ac85913
ayn <ayn.khatib@gmail.com>
2026-08-08 00:35:26 +0300

    Fix treesitter warnings
    Signed-off-by: ayn <ayn.khatib@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,41 @@ return {
 		build = ":TSUpdate",
 		lazy = false,
 		config = function()
+			-- START: Neovim 0.12 compatibility patch for nvim-treesitter
+			local query = require("vim.treesitter.query")
+			local old_add_dir = query.add_directive
+			local old_add_pred = query.add_predicate
+
+			local function unwrap(match)
+				local res = {}
+				for k, v in pairs(match) do
+					res[k] = type(v) == "table" and v[1] or v
+				end
+				return res
+			end
+
+			query.add_directive = function(name, handler, opts)
+				old_add_dir(name, function(match, pattern, bufnr, pred, metadata)
+					return handler(unwrap(match), pattern, bufnr, pred, metadata)
+				end, opts)
+			end
+
+			query.add_predicate = function(name, handler, opts)
+				old_add_pred(name, function(match, pattern, bufnr, pred)
+					return handler(unwrap(match), pattern, bufnr, pred)
+				end, opts)
+			end
+			
+			-- This require will now register nvim-treesitter's directives using our wrapped functions
+			local ts = require("nvim-treesitter")
+			
+			-- Restore the original functions to avoid breaking Neovim core
+			query.add_directive = old_add_dir
+			query.add_predicate = old_add_pred
+			-- END: patch
+
 			-- Specify which parsers to ensure are installed
-			require("nvim-treesitter").install({
+			ts.install({
 				"go",
 				"gomod",
 				"gowork",
